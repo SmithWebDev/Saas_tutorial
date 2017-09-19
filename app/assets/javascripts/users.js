80b38a4211e5077826857bc5b5804a2ae2086c1d
DB Smith <smithwebdd@gmail.com>
2017-09-19 22:02:11 +0000

    Added Stripe response handler and error handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,6 +11,7 @@ $(document).on('turbolinks:load', function(){
     
   //prevent default submission behavior
   event.preventDefault();
+  submitBtn.val('Processing').prop('disabled', true);
   
   //Collect card fields.
   var ccNum = $('#card_number').val(),
@@ -18,6 +19,34 @@ $(document).on('turbolinks:load', function(){
       expMon = $('card_month').val(),
       expYear = $('card_year').val();
       
+  //Use Stripe JS library to check for card errors
+  var error = false;
+  
+  //Validate card number
+  if( !Stripe.card.validateCardNumber(ccNum)){
+    error = true;
+    alert('The credit card number appears to be invalid');
+  }
+  
+  //Validate card number
+  if( !Stripe.card.validateCVC(cvcNum)){
+    error = true;
+    alert('The CVC number appears to be invalid');
+  }
+  
+  //Validate card number
+  if( !Stripe.card.validateExpiry(expMon, expYear)){
+    error = true;
+    alert('The expiration date appears to be invalid');
+  }
+  
+  
+  if (error){
+    //If there are card error, dont send to stripe
+    submitBtn.prop('disabled', false).val('Sign Up')
+  }
+  else{
+    
   //Send the card info to Stripe.
   Stripe.createToken({
     number: ccNum,
@@ -25,9 +54,22 @@ $(document).on('turbolinks:load', function(){
     exp_month: expMon,
     exp_year: expYear
   }, stripeResponseHandler);
+  }
+  
+  
+  return false;
   });
+  
   //Stripe returns card token
-  //nject card token as hidden field into form.
-  //Submit form to our Rails app
+  function stripeResponseHandler(status, response){
+    //Get Token from response
+    var token = response.id;
+    
+    //Inject card token as hidden field into form.
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    
+    //Submit form to our Rails app
+    theForm.get(0).submit();
+  }
 
 });
